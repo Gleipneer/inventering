e0ad1bf64d928bbe2d8823ec81d315a31ebfc2a1
Aaren Bertilsson <aarenbs@gmail.com>
2026-06-03 14:30:25 +0200

    Kolla om någon är inloggad på en klient i Get-InactiveClients

--- src/modules/InactiveClients.psm1
+++ src/modules/InactiveClients.psm1
@@ -6,7 +6,21 @@ function Get-InactiveClient {
 
     process {
         foreach ($computer in $Computers) {
-            $computer
+
+            try {
+                $cs = Get-CimInstance `
+                    -ClassName Win32_ComputerSystem `
+                    -ComputerName $computer.ComputerName `
+                    -ErrorAction Stop
+
+                # Returnera bara klienter där ingen är inloggad
+                if ([string]::IsNullOrWhiteSpace($cs.UserName)) {
+                    $computer
+                }
+            }
+            catch {
+                Write-Output "Error, kunde ej slå upp någon dator"
+            }
         }
     }
 }
